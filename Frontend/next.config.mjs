370d9da5c29431511bc636ccf54cdf0185283942
Marcos Caccavaio <mcaccavaio@quikforms.com>
2025-10-22 20:09:38 -0300

    adding docker configuration

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5082',
   },
